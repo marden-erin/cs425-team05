57e042be4034c0a31b714003de82996b1f4a6768
hieronymus-jodi <jhieronymus@nevada.unr.edu>
2022-11-22 16:54:06 -0800

    Setup React

---
+++ src/ExamplePage.js
@@ -0,0 +1,21 @@
+function ExamplePage() {
+  return (
+    <div className="ExamplePage">
+      <header className="ExamplePage-header">
+        <p>
+          Hello Brainy-ators!!
+        </p>
+        <a
+          className="ExamplePage-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Some info on React for y'all
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default ExamplePage;
