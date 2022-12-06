611df9470e22eeba2b846533975548ef94262261
hieronymus-jodi <jhieronymus@nevada.unr.edu>
2022-12-06 14:30:31 -0800

    Add logo

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import PageWrapper from '../components/complex-components/PageWrapper/PageWrapper'; // *TODO: Fix so it imports from components
 import { P } from '../components';
 import { COLORS } from '../constants';
+import Logo from '../imgs/logo.png';
 
 const TEMP_DIV = styled.div`
   width: 600px;
@@ -13,10 +14,12 @@ const TEMP_DIV = styled.div`
   background-color: ${COLORS.WHITE};
 `;
 
-// *TODO: Remove header when logo is added
 function Home() {
   return (
-    <PageWrapper pageTitle="OuterWhorld" header="OuterWhorld">
+    <PageWrapper pageTitle="OuterWhorld">
+      <h1>
+          <img src={Logo} alt="OuterWhorld" width="700" />
+      </h1>
       <TEMP_DIV>
         <P>Content goes here!</P>
       </TEMP_DIV>
