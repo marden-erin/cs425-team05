5b0df1e4d760630cda99982dee5a83f04740b0f1
hieronymus-jodi <jhieronymus@nevada.unr.edu>
2022-12-06 14:00:46 -0800

    Ensure wrapper functions properly

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,25 @@
 // TODO: Delete this file when actual pages are added
 import React from 'react';
+import styled from 'styled-components';
 
 import PageWrapper from '../components/complex-components/PageWrapper/PageWrapper'; // *TODO: Fix so it imports from components
 import { P } from '../components';
+import { COLORS } from '../constants';
+
+const TEMP_DIV = styled.div`
+  width: 600px;
+  height: 600px;
+
+  background-color: ${COLORS.WHITE};
+`;
 
 // *TODO: Remove pageTitle when logo is added
 function Home() {
   return (
     <PageWrapper pageTitle="OuterWhorld" header="OuterWhorld">
-      <P>Content goes here!</P>
+      <TEMP_DIV>
+        <P>Content goes here!</P>
+      </TEMP_DIV>
     </PageWrapper>
   );
 }
