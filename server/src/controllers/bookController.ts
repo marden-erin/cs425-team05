3e22295597a18f63cfe4896ac8fc6ddca1ffbb25
unknown <aiorgulescu.business@gmail.com>
2022-12-08 19:28:06 -0800

    Refactor getBooks API
    - API now checks if bookTitle is already in our database. If it is, we
      retrieve book info from database instead of google API
    
    - New books that are not in our database are added to our books table for faster retrieval in the future

--- server/src/controllers/bookController.ts
+++ server/src/controllers/bookController.ts
@@ -3,44 +3,64 @@ import asyncHandler from 'express-async-handler'
 import fetch from 'node-fetch'
 import { HTTPStatus } from '../utils/Enums'
 import type { Book } from '../utils/Types'
+import db from '../model/database'
 
-
-const getBook =  asyncHandler(async (req : Request ,res : Response) => {
-    if (!req.params.bookTitle)
+const getBook =  asyncHandler(async (req : Request, res : Response) => {
+    if (!req.query.bookTitle)
     {
-        res.status(HTTPStatus.BAD)
-        throw new Error('Missing book title')
-    }
-    
-    const bookData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${req.params.bookTitle}&key=${process.env.BOOK_API_KEY}`)
-
-    const formattedBookData = await bookData.json()
-
-    if  (formattedBookData.items)
-    {
-        const firstTenBooks = formattedBookData.items.length >= 10 ? formattedBookData.items.slice(0,10) : formattedBookData.items.slice(0, formattedBookData.items.length)
-
-        let filteredTenBooks: Book[] = [];
-    
-        firstTenBooks.forEach((element: { [x: string]: any }) => {
-            filteredTenBooks.push(
-            {
-                title : element.volumeInfo.title,
-                authors : element.volumeInfo.authors,
-                pageCount: element.volumeInfo.pageCount,
-                description: element.volumeInfo.description,
-                covers: element.volumeInfo.imageLinks
-            }
-           )
-    
-        });
-    
-        res.status(HTTPStatus.OK).json(filteredTenBooks)
-        return;
+        const errMsg = 'Missing book title';
+        res.status(HTTPStatus.BAD).json(errMsg);
+        throw new Error(errMsg);
     }
 
-    res.status(HTTPStatus.BAD)
-    throw new Error('Retrieving book info failed')
+    const bookTitle = req.query.bookTitle;
+
+    const query = `select * from books where bookTitle='${bookTitle}';`;
+    const [books] : any[] = await db.promise().query(query);
+
+    if (books.length > 0)
+    {
+        console.log("IN");
+        res.status(HTTPStatus.OK).json(books);
+    }
+    else
+    {
+        const bookData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${process.env.BOOK_API_KEY}`)
+        const formattedBookData = await bookData.json();
+    
+        if  (formattedBookData.items)
+        {
+            const firstTenBooks = formattedBookData.items.length >= 10 ? formattedBookData.items.slice(0,10) : formattedBookData.items.slice(0, formattedBookData.items.length)
+    
+            const filteredTenBooks: Book[] = [];
+        
+            firstTenBooks.forEach((element: { [x: string]: any }) => {
+                const description = element.volumeInfo.description;
+                const edited = description.replace(/[']/g, "");
+
+                const query = `insert ignore into books(book_id, bookTitle, author, pageCount, description, bookCover) values(DEFAULT, '${element.volumeInfo.title}', '${element.volumeInfo.authors[0]}', '${element.volumeInfo.pageCount}', '${edited}', '${element.volumeInfo.imageLinks.thumbnail}');`;
+                db.query(query);
+
+                filteredTenBooks.push(
+                {
+                    title : element.volumeInfo.title,
+                    authors : element.volumeInfo.authors,
+                    pageCount: element.volumeInfo.pageCount,
+                    description: element.volumeInfo.description,
+                    covers: element.volumeInfo.imageLinks
+                }
+               )
+            });
+        
+            res.status(HTTPStatus.OK).json(filteredTenBooks);
+        }
+        else
+        {
+            const errMsg ='Retrieving book info failed';
+            res.status(HTTPStatus.BAD).json(errMsg);
+            throw new Error(errMsg);
+        }
+    }
 })
 
 
