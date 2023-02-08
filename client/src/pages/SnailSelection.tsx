4b94d4eed8a755a4c1808d490cac6835bf293fb9
hieronymus-jodi <jhieronymus@nevada.unr.edu>
2023-02-08 13:28:41 -0800

    Add continue button and header to Snail Selection

--- client/src/pages/SnailSelection.tsx
+++ client/src/pages/SnailSelection.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { PageWrapper, SnailSelectCard } from '../components';
+import { LargeRoundedButton, PageWrapper, SnailSelectCard } from '../components';
 
 const FlexBoxWrapper = styled.div`
-    height: 85vh;
+    height: 80vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    gap: 10rem;
 `;
 
 const Radio = styled.div`
@@ -19,13 +20,14 @@ const Radio = styled.div`
 
 function SnailSelection() {
     return (
-        <PageWrapper pageTitle="Select A Snail">
+        <PageWrapper pageTitle="Select A Snail" header="Select A Snail!">
             <FlexBoxWrapper>
                 <Radio>
                     <SnailSelectCard color='blue' name="snail-color"/>
                     <SnailSelectCard color='pink' name="snail-color"/>
                     <SnailSelectCard color='yellow' name="snail-color"/>
                 </Radio>
+                <LargeRoundedButton>Continue</LargeRoundedButton>
             </FlexBoxWrapper>
         </PageWrapper>
     );
