33ea8d1ff6188b723f31d5a21dcae10fbd6bf6bf
unknown <aiorgulescu.business@gmail.com>
2023-03-27 11:49:40 -0700

    Add updateGrave and deleteGrave Functionality

--- server/src/controllers/graveyardController.ts
+++ server/src/controllers/graveyardController.ts
@@ -90,13 +90,54 @@ const createGrave = asyncHandler(async (req: Request, res: Response) => {
 });
 
 const updateGrave = asyncHandler(async (req: Request, res: Response) => {
-	console.log("in update grave");
-	res.status(HTTPStatus.OK).json("in update grave");
+	const { graveyard_id, gravestone, grave_type } = req.body;
+
+	if (graveyard_id && gravestone && grave_type) {
+		const filteredGravestone = gravestone.replace(/"/g, "''");
+		try {
+			const query = `update Graveyard set gravestone="${filteredGravestone}", grave_type=${grave_type} where graveyard_id=${graveyard_id}`;
+			await db.promise().query(query);
+
+			res.status(HTTPStatus.OK).json("Successfully updated grave");
+		} catch (err: any) {
+			res.status(HTTPStatus.BAD).json(err.sqlMessage);
+			throw new Error(err);
+		}
+	} else {
+		const errMsg =
+			"Error. Missing one or more params (graveyard_id, gravestone, grave_type)";
+		res.status(HTTPStatus.BAD).json(errMsg);
+		throw new Error(errMsg);
+	}
 });
 
 const deleteGrave = asyncHandler(async (req: Request, res: Response) => {
-	console.log("in delete grave");
-	res.status(HTTPStatus.OK).json("in delete grave");
+	const { graveyard_id } = req.body;
+
+	if (graveyard_id) {
+		try {
+			let query = `select * from Graveyard where graveyard_id=${graveyard_id}`;
+			const [grave]: any[] = await db.promise().query(query);
+
+			if (grave.length > 0) {
+				query = `delete from Graveyard where graveyard_id=${graveyard_id}`;
+				await db.promise().query(query);
+
+				res.status(HTTPStatus.OK).json("Successfully deleted grave");
+			} else {
+				res
+					.status(HTTPStatus.BAD)
+					.json(`Error. Grave with id ${graveyard_id} does not exist`);
+			}
+		} catch (err: any) {
+			res.status(HTTPStatus.BAD).json(err.sqlMessage);
+			throw new Error(err);
+		}
+	} else {
+		const errMsg = "Error. Missing one or more params (graveyard_id)";
+		res.status(HTTPStatus.BAD).json(errMsg);
+		throw new Error(errMsg);
+	}
 });
 
 export { getGrave, getAllGraves, createGrave, updateGrave, deleteGrave };
